f186191e32a26da093f1335af20e222f6e393cf9
Eric Doering <ericbenjamindoering@gmail.com>
2025-03-20 13:28:11 -0400

    image sliders added for customization

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import requests
 import io
-from PIL import Image
+from PIL import Image, ImageEnhance
 from variables import API_KEY  
 
 API_URL = "https://api-inference.huggingface.co/models/sd-community/sdxl-flash"
@@ -25,15 +25,22 @@ def query(payload):
         print("Error processing response:", e)
         return None
 
-def generate_image(prompt):
+def generate_image(prompt, brightness, contrast):
     image_bytes = query({ "inputs": prompt })
     image = Image.open(io.BytesIO(image_bytes))
+
+    enhancer = ImageEnhance.Brightness(image)
+    image = enhancer.enhance(brightness)
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(contrast)
+
     return image
 
 demo = gr.Interface(
     fn=generate_image,
-    inputs="text",
-    outputs="image"
+    inputs=["text", gr.Slider(0.5, 2.0, 1.0), gr.Slider(0.5, 2.0, 1.0)],
+    outputs="image",
+    live=True
 )
 
 demo.launch(share=True)
